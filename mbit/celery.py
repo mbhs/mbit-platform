f77d356d0bcb86a09303b68056f9692e8952a020
Kevin Higgs <github@kevinhiggs.com>
2019-11-01 18:32:59 -0400

    update

--- mbit/celery.py
+++ mbit/celery.py
@@ -6,5 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mbit.settings')
 
 app = Celery('mbit')
 app.conf.broker_url = 'redis://'
+app.conf.worker_prefetch_multiplier = 1
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
