0c048a94fef5ace599a9bf6e4427f6a08ca74dca
Kevin Higgs <github@kevinhiggs.com>
2019-11-01 21:46:31 -0400

    logout redirect

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,6 @@ app_name = 'dashboard'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('login', auth_views.LoginView.as_view(template_name='dashboard/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(next_page='https://mbit.mbhs.edu')),
+    path('logout', auth_views.LogoutView.as_view(next_page='/login')),
     path('submission/<int:id>/<str:filename>', views.submission)
 ]
