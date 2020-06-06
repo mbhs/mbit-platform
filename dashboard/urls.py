bccbecff881d9ef8cdae9372bb17897e506dea61
Kevin Higgs <github@kevinhiggs.com>
2020-06-06 19:22:21 -0400

    change password

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('scores', views.scores, name='scores'),
 	path('login', auth_views.LoginView.as_view(template_name='dashboard/login.html', redirect_authenticated_user=True), name='login'),
 	path('logout', auth_views.LogoutView.as_view(next_page='/login')),
+	path('changepassword', auth_views.PasswordChangeView.as_view(template_name='dashboard/changepassword.html', success_url='/')),
 	path('register', views.register, name='register'),
 	path('submission/<int:id>/<str:filename>', views.submission)
 ]
